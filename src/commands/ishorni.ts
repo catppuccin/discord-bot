d46c6a45c00147ea1ce9ef39f4a42dc1c70e69ec
Ace Daksh <ektadaksh7@gmail.com>
2022-07-11 08:31:40 -0700

    fix: replace var keyword

---
+++ src/commands/ishorni.ts
@@ -0,0 +1,35 @@
+import { CommandInteraction } from "discord.js";
+import { Discord, Slash, SlashGroup, SlashOption } from "discordx";
+import { PrismaClient } from "@prisma/client";
+
+// @ts-ignore-error
+import { errorHandler } from "../utils/error_handler.ts";
+import { Interface } from "../db.ts";
+
+const prisma = new PrismaClient();
+const face = new Interface(prisma);
+
+/*  isHorni COMMAND
+ *  Description: Tells you if the current channel is horni.
+ */
+
+@Discord()
+@SlashGroup({ name: "channel" })
+export class ChannelCommands {
+	@Slash("horni")
+	@SlashGroup("channel")
+	async isHorni(interaction: CommandInteraction): Promise<void> {
+		const res = await face.isHorni(interaction.channelId);
+		if (res) {
+			return interaction.reply({
+				content: "Channel is horni.",
+				ephemeral: true,
+			});
+		} else {
+			return interaction.reply({
+				content: "Channel is *not* horni.",
+				ephemeral: true,
+			});
+		}
+	}
+}
